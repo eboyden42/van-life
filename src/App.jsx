9c192f597ec8b0981e586bfa52d83709e620ab86
Eli Boyden <aqu4eb@virginia.edu>
2025-05-19 10:08:29 -0400

    added Vans page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from "react-router"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from "./pages/Vans"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,11 +14,13 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/Vans.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Vans() {
+    return (
+        <>
+            <h1>Vans page goes here 🚐</h1>
+        </>
+    )
+}
